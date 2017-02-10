73c250012755cf7cc57ad6b64489e496cc49b2e9
Korn, Uwe <Uwe.Korn@blue-yonder.com>
2017-02-10 13:55:03 +0100

    Conditionally build on Linux with -O2

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,7 +11,14 @@ source activate "${CONDA_DEFAULT_ENV}"
 
 autoreconf --force --install
 ./autogen.sh
-CXXFLAGS="${CXXFLAGS} -O1 -DNDEBUG" ./configure --with-pic --prefix=$PREFIX --disable-dependency-tracking
+if [ "$(uname)" == "Linux" ]; then
+  export CXXFLAGS="${CXXFLAGS} -O2 -DNDEBUG"
+else
+  # There is at least on OSX a bug affecting -O2 builds
+  export CXXFLAGS="${CXXFLAGS} -O1 -DNDEBUG"
+fi
+
+./configure --with-pic --prefix=$PREFIX --disable-dependency-tracking
 make
 make check
 make install
