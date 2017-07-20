102f096e4fa217547352e45b0197196fd573a813
Dougal J. Sutherland <dougal@gmail.com>
2017-07-20 14:49:33 +0100

    update to 1.1.6
    - Change to cmake build instead of soon-to-be-depracated autotools.
    
    - Static lib seems to be no longer supported, or at least isn't in the
      cmake configuration (so if that matters we'd have to patch it or stick
      to autotools). It's no longer in the package, and I removed the test
      that it's present. Does anyone use it? CC @wesm, who added the flag to
      use static linking (78f1c05).
    
    - The patch is to make `snappy-unittest`, the new equivalent to the old
      `make check`, not run the slow-ish microbenchmarks but just the quick
      unit tests (without futzing around with gflags, which didn't work for
      me when I just ran `conda install gflags`.).
    
    - I removed the -O1 thing on OSX. The comment says there's a bug, but
      the unit tests at least worked for me, and since snappy seems to not
      have a bug tracker, who knows?

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,24 +1,18 @@
 #!/bin/env bash
+set -e
 
+mkdir build
+cd build
+cmake .. \
+    -DCMAKE_INSTALL_PREFIX="$PREFIX" \
+    -DCMAKE_PREFIX_PATH="$PREFIX" \
+    -DCMAKE_BUILD_TYPE=Release
 
-# FIXME: This is a hack to make sure the environment is activated.
-# The reason this is required is due to the conda-build issue
-# mentioned below.
-#
-# https://github.com/conda/conda-build/issues/910
-#
-source activate "${CONDA_DEFAULT_ENV}"
+make -j $CPU_COUNT
 
-autoreconf --force --install
-./autogen.sh
-if [ "$(uname)" == "Linux" ]; then
-  export CXXFLAGS="${CXXFLAGS} -O2 -DNDEBUG"
-else
-  # There is at least on OSX a bug affecting -O2 builds
-  export CXXFLAGS="${CXXFLAGS} -O1 -DNDEBUG"
-fi
+# need to be in the root directory for this to run properly
+cd ..
+build/snappy-unittest
+cd build
 
-./configure --with-pic --prefix=$PREFIX --disable-dependency-tracking
-make
-make check
 make install
