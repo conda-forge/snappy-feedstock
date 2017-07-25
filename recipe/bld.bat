0ef07b7b7a987f356d26e835fc1a33794fcdcf8b
Dougal J. Sutherland <dougal@gmail.com>
2017-07-25 14:31:14 +0100

    set CMAKE_POSITION_INDEPENDENT_CODE

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -7,6 +7,7 @@ cmake -G "NMake Makefiles" ^
     -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
     -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
     -DCMAKE_BUILD_TYPE:STRING=Release ^
+    -DCMAKE_POSITION_INDEPENDENT_CODE=1 ^
     ..
 if errorlevel 1 exit 1
 
