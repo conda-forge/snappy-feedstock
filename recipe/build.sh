31b043260f9d4ee40c1a04de2623d8c98342c182
Dougal J. Sutherland <dougal@gmail.com>
2017-07-25 14:49:16 +0100

    cmake-based build on unix too

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,24 +1,26 @@
 #!/bin/env bash
+set -e
 
-
-# FIXME: This is a hack to make sure the environment is activated.
-# The reason this is required is due to the conda-build issue
-# mentioned below.
-#
-# https://github.com/conda/conda-build/issues/910
-#
-source activate "${CONDA_DEFAULT_ENV}"
-
-autoreconf --force --install
-./autogen.sh
-if [ "$(uname)" == "Linux" ]; then
-  export CXXFLAGS="${CXXFLAGS} -O2 -DNDEBUG"
+if [[ "$(uname)" == "Darwin" ]]; then
+    # at least for Travis's compiler, -O2 fails the unit tests
+    export CXXFLAGS="${CXXFLAGS} -O1"
 else
-  # There is at least on OSX a bug affecting -O2 builds
-  export CXXFLAGS="${CXXFLAGS} -O1 -DNDEBUG"
+    export CXXFLAGS="${CXXFLAGS} -O2"
 fi
+export CXXFLAGS="${CXXFLAGS} -DNDEBUG"
 
-./configure --with-pic --prefix=$PREFIX --disable-dependency-tracking
-make
-make check
+mkdir build
+cd build
+cmake .. \
+    -DCMAKE_INSTALL_PREFIX="$PREFIX" \
+    -DCMAKE_PREFIX_PATH="$PREFIX" \
+    -DCMAKE_POSITION_INDEPENDENT_CODE=1
+
+make -j $CPU_COUNT
+
+# need to be in the root directory for this to run properly
+cd ..
+build/snappy-unittest
+cd build
+
 make install
