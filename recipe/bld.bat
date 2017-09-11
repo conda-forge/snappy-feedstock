7951244e0d94ea648f07bdde8a9df6c1ef860717
Dougal J. Sutherland <dougal@gmail.com>
2017-09-11 16:14:26 +0100

    update to 1.1.7

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,7 +1,31 @@
 setlocal EnableDelayedExpansion
 
-mkdir build
-cd build
+mkdir build-dynamic
+cd build-dynamic
+
+cmake -G "NMake Makefiles" ^
+    -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
+    -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
+    -DCMAKE_BUILD_TYPE:STRING=Release ^
+    -DBUILD_SHARED_LIBS=ON ^
+    ..
+if errorlevel 1 exit 1
+
+nmake
+if errorlevel 1 exit 1
+
+:: need to be in the root directory for this to run properly
+cd ..
+build-dynamic\snappy_unittest
+if errorlevel 1 exit 1
+cd build-dynamic
+
+nmake install
+if errorlevel 1 exit 1
+
+cd ..
+mkdir build-static
+cd build-static
 
 cmake -G "NMake Makefiles" ^
     -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
@@ -16,9 +40,9 @@ if errorlevel 1 exit 1
 
 :: need to be in the root directory for this to run properly
 cd ..
-build\snappy-unittest
+build-static\snappy_unittest
 if errorlevel 1 exit 1
-cd build
+cd build-static
 
 nmake install
 if errorlevel 1 exit 1
