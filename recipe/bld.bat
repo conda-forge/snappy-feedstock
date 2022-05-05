175d260ce662cc04ad1f57ee36088858c8d9fab8
Uwe L. Korn <uwe.korn@quantco.com>
2022-05-05 21:38:14 +0200

    Build script fixes

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -11,6 +11,7 @@ cmake -G "NMake Makefiles" ^
     -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
     -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
     -DCMAKE_BUILD_TYPE:STRING=Release ^
+    -DSNAPPY_BUILD_BENCHMARKS=OFF ^
     -DBUILD_SHARED_LIBS=ON ^
     ..
 if errorlevel 1 exit 1
